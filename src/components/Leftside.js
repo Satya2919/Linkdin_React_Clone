211e30dc64fe93f41fe50356eff59adf8c6be0ee
satyam <sat.tiwari21@gmail.com>
2021-05-16 15:51:06 +0530

    added main

---
+++ src/components/Leftside.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Leftside =(props) =>{
+    return (
+        <Container>
+            LeftSide
+        </Container>
+    );
+};
+
+const Container = styled.div`
+ grid-area: leftside;
+`;
+
+export default Leftside;
